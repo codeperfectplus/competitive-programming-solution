4ed48e2ed7a7a9b05d6dbf6511cf7a9ef6c96a04
codeperfectplus <deepak008@live.com>
2020-08-17 12:37:39 +0530

    new commit

--- Excercism.IO/python/high-scores/high_scores_test.py
+++ Excercism.IO/python/high-scores/high_scores_test.py
@@ -17,8 +17,8 @@ class HighScoresTest(unittest.TestCase):
         self.assertEqual(personal_best(scores), expected)
 
     def test_personal_top_three_from_a_list_of_scores(self):
-        scores = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70]
-        expected = [100, 90, 70]
+        scores = [1, 5, 3, 2]
+        expected = [5, 3, 2]
         self.assertEqual(personal_top_three(scores), expected)
 
     def test_personal_top_highest_to_lowest(self):
---
+++ Project Euler/Python/Power digit sum.py
@@ -0,0 +1,7 @@
+answer = pow(2,1000)
+
+sum = 0
+
+for i in str(answer):
+    sum += int(i)
+print(sum)
