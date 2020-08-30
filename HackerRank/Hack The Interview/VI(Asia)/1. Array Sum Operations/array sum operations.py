b122519d94439f538916680e55ee5de1913c6157
codeperfectplus <deepak008@live.com>
2020-08-30 23:06:24 +0530

    :heavy_plus_sign: Hack the Interview added

---
+++ HackerRank/Hack The Interview/VI(Asia)/1. Array Sum Operations/array sum operations.py
@@ -0,0 +1,17 @@
+def performOperations(N, op):
+    # Write your code here
+    # print(N, op)
+    a = [i for i in range(1, N + 1)]
+    s = sum(a)
+    ans = []
+    for i in op:
+        if i in a:
+            a[0], a[-1] = a[-1], a[0]
+        else:
+            s -= a[-1]
+            s += i
+            a.pop()
+        ans.append(s)
+    return ans
+
+print(performOperations(3, [4, 2]))
