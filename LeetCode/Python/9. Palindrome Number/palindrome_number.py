2c0ecfd79bf4d11994e3279c2025e11449dc050c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-18 00:59:10 +0800

    Delete LeetCode/Python directory

--- LeetCode/Python/9. Palindrome Number/palindrome_number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x >= 0:
-            a = int(str(x)[::-1])
-            if a == x:
-                return True
-            return False
-        return False
